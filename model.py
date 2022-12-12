a66094a6fc66af276d9ae31b44365e151d5940e1
Kellian <kellian.cottart@edu.ece.fr>
2022-12-12 17:41:13 +0100

    feat: added working launch

--- model.py
+++ model.py
@@ -15,8 +15,11 @@ import numpy as np
 import os
 import torch
 from models.nice import NICE
+from models.nice.utils import Trainer
 
 # <!> DO NOT ADD ANY OTHER ARGUMENTS <!>
+
+
 def generative_model(noise):
     """
     Generative model
@@ -28,23 +31,26 @@ def generative_model(noise):
     """
     # See below an example
     # ---------------------
-    latent_variable = noise[:, :15]  # use the first 10 dimensions of the noise in this example
-
-    # load my parameters (of dimension 15 in this example). 
+    len_dim = 10
+    # use the first 10 dimensions of the noise in this example
+    latent_variable = noise[:, :len_dim]
+    # load my parameters (of dimension 15 in this example).
     # <!> be sure that they are stored in the parameters/ directory <!>
-    model = NICE(prior=latent_variable, 
-            coupling=4, 
-            len_input=15, 
-            mid_dim=15, 
-            hidden=4, 
-            mask_config=1)
-    model.load_state_dict(torch.load("./parameters/nice/model_1.pt"))
-    model.eval()
+    model = NICE(prior=torch.distributions.Normal(torch.tensor(0.), torch.tensor(1.)),
+                 coupling=4,
+                 len_input=len_dim,
+                 mid_dim=10,
+                 hidden=4,
+                 mask_config=1.)
+    trainer = Trainer(model)
+    model_trained = trainer.load_model("parameters/nice/model_1.pt")
+    # model_trained = trainer.load_model("./parameters/nice/model_1.pt")
+    model_trained.eval()
     # generate the output of the generative model
-    output = model(latent_variable)
-    
+    output = latent_variable.copy()
+    for i in range(latent_variable.shape[0]):
+        for j in range(latent_variable.shape[1]):
+            output[i][j] = np.mean(model_trained.g(
+                latent_variable[i][j]).cpu().detach().numpy())
     # return the output of the generative model
-    return output 
-
-
-
+    return output
