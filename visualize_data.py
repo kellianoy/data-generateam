a14b987ec16ecbd1fbdee1f78d2cdb44ba8ff960
Kellian <kellian.cottart@edu.ece.fr>
2022-12-19 20:06:33 +0100

    feat: visualize_data for nice_conditional

--- visualize_data.py
+++ visualize_data.py
@@ -1,23 +1,23 @@
 import matplotlib.pyplot as plt
-from data.dataset_tools import generate_basic_timeseries_splitted_normalized_dataset
+from data.dataset_tools import generate_basic_timeseries_splitted_normalized_dataset_with_month_info
 import torch
 import numpy as np
 from metrics import Metrics
 
 if __name__ == "__main__":
 
-    model_type = "nice"
-    model_name = "model_1"
+    model_type = "nice_conditional"
+    model_name = "nc_optimal"
 
     if model_type == "simple_gan":
         from parameters.simple_gan import Model
-        from parameters.simple_gan.utils import Trainer
+        from parameters.simple_gan import Trainer
         model = Model()
         trainer = Trainer(model)
 
     if model_type == "nice":
         from parameters.nice import NICE
-        from parameters.nice.utils import Trainer
+        from parameters.nice import Trainer
         noise_input = torch.distributions.Normal(
             torch.tensor(0.), torch.tensor(1.))
 
@@ -35,10 +35,31 @@ if __name__ == "__main__":
                      mask_config=mask_config)
         trainer = Trainer(model)
 
+    if model_type == "nice_conditional":
+        from parameters.nice_conditional import NICE_CONDITIONAL
+        from parameters.nice_conditional import Trainer
+        noise_input = torch.distributions.Normal(
+            torch.tensor(0.), torch.tensor(1.))
+
+        coupling = 6
+        len_input_output = 10
+        mid_dim = 10
+        hidden = 4
+        mask_config = 1.
+
+        model = NICE_CONDITIONAL(prior=noise_input,
+                                 coupling=coupling,
+                                 len_input=len_input_output,
+                                 mid_dim=mid_dim,
+                                 hidden=hidden,
+                                 mask_config=mask_config,
+                                 time_dim=13)
+        trainer = Trainer(model)
+
     model_path = "parameters/{}/models_saved/{}.pt".format(
         model_type, model_name)
-    dataset = generate_basic_timeseries_splitted_normalized_dataset(
-        "df_train", proportion_test=1.)
+    dataset = generate_basic_timeseries_splitted_normalized_dataset_with_month_info(
+        "df_train", proportion_test=0.8)
     training_set = dataset[0][0]
     testing_set = dataset[0][1]
     max_temperature = dataset[1]
@@ -51,6 +72,9 @@ if __name__ == "__main__":
     n_test = 10000
 
     generated_sample = trainer.generate_sample(n_test, time_interval)
+
+    testing_distribution = generate_basic_timeseries_splitted_normalized_dataset(
+        "df_train", proportion_test=0.8)[0][1]
 
     fig, axs = plt.subplots(nrows=10, ncols=2, figsize=(10, 30))
     fig.subplots_adjust(hspace=.5, wspace=0.5)
